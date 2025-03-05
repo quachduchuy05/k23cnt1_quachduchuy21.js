d5c2a4c24ed7b50d05a369bdab8fcffa058f32eb
quachduchuy05 <huychuoi05@gmail.com>
2025-03-05 21:48:14 +0700

    First commit - up bài lên GitHub

--- huy-day7/src/components/QdhLoginComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function QdhLoginComp() {
-  return (
-    <div>well cone huy chuoi</div>
-  )
-}
--- huy-day7/src/components/QdhlogoutComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function QdhlogoutComp() {
-  return (
-    <div>goodbye-please sign in </div>
-  )
-}
